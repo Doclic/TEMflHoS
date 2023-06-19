7d4b48dd51960d9ccdec9300ed54cb597ee7b690
BlendyDev <blendydev@gmail.com>
2023-06-19 20:40:09 +0200

    Swapped order of the module command, adding support for commands without an input module

--- src/main/kotlin/me/doclic/temflhos/module/ModuleManager.kt
+++ src/main/kotlin/me/doclic/temflhos/module/ModuleManager.kt
@@ -1,14 +1,14 @@
 package me.doclic.temflhos.module
 
 object ModuleManager {
-    private val modules = HashMap<String, Module>()
+    val modules = HashMap<String, Module>()
 
     fun registerModule(module: Module) {
         if(hasModule(module)) return
 
         modules.put(module.id, module)
     }
-    fun hasModule(module: Module): Boolean { return hasModule(module.id) }
-    fun hasModule(id: String): Boolean { return modules.containsKey(id) }
-    fun getModule(id: String): Module? { return modules[id] }
+    fun hasModule(module: Module): Boolean = hasModule(module.id)
+    fun hasModule(id: String): Boolean = modules.containsKey(id)
+    fun getModule(id: String): Module? = modules[id]
 }
