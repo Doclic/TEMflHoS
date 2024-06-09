b61fb1cb8b6b41255c67529a962f83005b6d2740
BlendyDev <blendydev@gmail.com>
2024-06-10 00:59:52 +0200

    style: replace mc.thePlayer by localPlayer

--- src/main/kotlin/me/doclic/temflhos/module/SafeInteractModule.kt
+++ src/main/kotlin/me/doclic/temflhos/module/SafeInteractModule.kt
@@ -2,7 +2,6 @@ package me.doclic.temflhos.module
 
 import me.doclic.temflhos.event.C2SPacketEvent
 import me.doclic.temflhos.util.localPlayer
-import me.doclic.temflhos.util.mc
 import me.doclic.temflhos.util.tChat
 import net.minecraft.block.Block
 import net.minecraft.block.material.Material
@@ -23,9 +22,9 @@ object SafeInteractModule : Module("safe_interact", "Safe Interact", keyCode = K
     private fun preventDragonEggInteract(e: C2SPacketEvent) {
         if (!listOf(C07PacketPlayerDigging::class, C08PacketPlayerBlockPlacement::class).contains(e.packet::class)) return
         val material: Material = if (e.packet is C07PacketPlayerDigging) {
-            mc.thePlayer.entityWorld.getBlockState((e.packet as C07PacketPlayerDigging).position).block.material
+            localPlayer.entityWorld.getBlockState((e.packet as C07PacketPlayerDigging).position).block.material
         } else
-            mc.thePlayer.entityWorld.getBlockState((e.packet as C08PacketPlayerBlockPlacement).position).block.material
+            localPlayer.entityWorld.getBlockState((e.packet as C08PacketPlayerBlockPlacement).position).block.material
         if (material != Material.dragonEgg) return
         tChat("${EnumChatFormatting.GREEN}Safe interact prevented you from teleporting that block")
         e.cancelled = true
